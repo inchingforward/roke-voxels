e604ea7874c5c2ccaabebf61e3b7f4c18a26bf49
mjanger <inchingforward@gmail.com>
2018-05-31 13:27:45 -0500

    Added three.js and initialization code.

--- roke.go
+++ roke.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/gopherjs/gopherjs/js"
+)
+
 func main() {
-	println("Hello, world!")
+	var document = js.Global.Get("document")
+	var body = document.Get("body")
+
+	var three = js.Global.Get("THREE")
+	//var scene = three.Get("Scene").New()
+	//var camera = three.Get("PerspectiveCamera").New(75, 500, 0.1, 1000.0)
+	var renderer = three.Get("WebGLRenderer").New()
+
+	renderer.Call("setSize", 500, 500)
+
+	body.Call("appendChild", renderer.Get("domElement"))
+
 }
